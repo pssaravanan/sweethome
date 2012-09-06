4e8f26e77c4b67aa483a9d84b7280683b1841ecb
P S Saravanan <saravanp@thoughtworks.com>
2012-09-06 23:40:42 +0530

    Saravanan | Initial set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sweethome::Application.config.secret_token = '482ca0fe9b61c5f5868cee0970bc27451a41e48b1be55d2f9c5c2fc3a22ad709e36a98ce4e32bc495dab6bca0d82b29009c08a9f4fd62a658a6fb9d648287b20'
